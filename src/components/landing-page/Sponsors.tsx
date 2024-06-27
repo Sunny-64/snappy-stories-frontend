1f617afa8e2941bd3089458f6e86a21a00f23afc
Sunny <sunny6464n@gmail.com>
2024-06-27 15:53:44 +0530

    remove build error

--- src/components/landing-page/Sponsors.tsx
+++ src/components/landing-page/Sponsors.tsx
@@ -24,8 +24,8 @@ const Sponsors = () => {
         <p className='text-3xl md:text-4xl lg:text-5xl font-bold text-center mb-6'><span className='text-complement'>Our</span> Sponsors</p>
          <div className='flex justify-center flex-wrap md:justify-between gap-4 lg:mt-14'>
          {sponsors_data.map((sponsor, index) => (
-          <div className='w-[100px] h-[100px] md:w-[140px] md:h-[100px] lg:w-[150px] lg:h-[200px] relative'>
-              <Image key={index} className='w-full h-full  object-contain' src={sponsor.img} alt={'sponsor'} fill/>
+          <div key={index} className='w-[100px] h-[100px] md:w-[140px] md:h-[100px] lg:w-[150px] lg:h-[200px] relative'>
+              <Image className='w-full h-full  object-contain' src={sponsor.img} alt={'sponsor'} fill/>
           </div>
           ))}
          </div>
